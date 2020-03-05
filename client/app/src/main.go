7b005626cae7238e5bd12da75dbfa2c8a35a977d
Marcelo Marques <marcelo@smarques.com>
2020-03-05 04:19:01 -0300

    First Commit

---
+++ client/app/src/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"database/sql"
+	"html/template"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+func dbConnect() *sql.DB {
+        connection := "user=pguser dbname=application1 password=pgpass host=db sslmode=disable"
+        db, err := sql.Open("postgres", connection)
+        if err != nil {
+                panic(err.Error())
+        }
+        return db
+}
+
+type Customer struct {
+        Id int
+        Name string
+        Email string
+}
+
+var temp = template.Must(template.ParseGlob("src/templates/*.html"))
+
+func main() {
+		http.HandleFunc("/", index)
+        http.ListenAndServe(":8081", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request){
+	db := dbConnect()
+	selectAll, err := db.Query("select * from customers")
+	if err != nil {
+			panic(err.Error())
+	}
+
+	c := Customer{}
+	customers := []Customer{}
+
+	for selectAll.Next(){
+			var id int
+			var name, email string
+			err := selectAll.Scan(&id, &name, &email)
+			if err != nil {
+					panic(err.Error())
+			}
+
+			c.Name = name
+			c.Email = email
+
+			customers = append(customers, c)
+
+	}
+
+	temp.ExecuteTemplate(w, "Index", customers)
+	defer db.Close()
+}
